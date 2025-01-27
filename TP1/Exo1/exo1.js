a7c38496dee4359c1caba686726f109a324079f7
William Brooks <william.brooks@etu.unilim.fr>
2025-01-27 09:48:27 +0100

    Initial commit

---
+++ TP1/Exo1/exo1.js
@@ -0,0 +1,36 @@
+import * as THREE from 'three';
+
+
+// Scene
+const scene = new THREE.Scene();
+
+
+// Sphere
+const geometry = new THREE.SphereGeometry(3, 16, 16);
+const material = new THREE.MeshBasicMaterial({
+   color: 0xffffff,
+   wireframe: true,
+});
+const mesh = new THREE.Mesh(geometry, material);
+scene.add(mesh);
+
+
+// Light
+const light = new THREE.PointLight(0xffffff, 1, 100);
+light.position.set(0, 10, 10);
+scene.add(light);
+const aLight = new THREE.AmbientLight(0x151515);
+scene.add(aLight);
+
+
+// Camera
+const camera = new THREE.PerspectiveCamera(45, 800 / 600);
+camera.position.z = 20;
+scene.add(camera);
+
+
+// Renderer
+const canvas = document.querySelector(".webgl");
+const renderer = new THREE.WebGLRenderer({ canvas });
+renderer.setSize(800, 600);
+renderer.render(scene, camera);
